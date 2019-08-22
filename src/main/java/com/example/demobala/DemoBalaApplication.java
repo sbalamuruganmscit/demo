6f9838473b603276a21a95fea04665c140aea4bc
sbalamuruganmscit <sbalamuruganmscit@gmail.com>
2019-08-22 08:48:08 +0530

    demo

---
+++ src/main/java/com/example/demobala/DemoBalaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demobala/DemoBalaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demobala;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoBalaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
